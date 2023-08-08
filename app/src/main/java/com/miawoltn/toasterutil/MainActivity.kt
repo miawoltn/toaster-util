1f6e1068b1f1243272f829859b72bf7fa50d5d20
miawoltn <amnabdulmlaik@gmail.com>
2023-08-08 12:22:59 +0100

    another build fix

--- app/src/main/java/com/miawoltn/toasterutil/MainActivity.kt
+++ app/src/main/java/com/miawoltn/toasterutil/MainActivity.kt
@@ -2,6 +2,7 @@ package com.miawoltn.toasterutil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.miawoltn.toasterlibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
